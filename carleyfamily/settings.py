73fce6cb8f405e93373aa150dbad550889542d69
Jeff Carley <jeff.carley@gmail.com>
2022-09-21 18:32:02 -0500

    Adds accounts app with CustomUser model

--- carleyfamily/settings.py
+++ carleyfamily/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # Local
     "photos.apps.PhotosConfig",
     "apis.apps.ApisConfig",
+    "accounts.apps.AccountsConfig",
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "accounts.CustomUser"
