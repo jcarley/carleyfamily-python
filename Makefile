b6c529aa7caa16a8d92c1fa190b6d46d68d7c144
Jeff Carley <jeff.carley@gmail.com>
2022-09-23 10:49:14 -0500

    updates

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
+dev:
+	uvicorn carleyfamily.asgi:application
+
+# how do we set the environment to production
 test:
 	python manage.py test
 
 watch-tests:
 	ls *.py | entr python manage.py test
+
+# how do we set the environment to production
+production:
+	uvicorn carleyfamily.asgi:application
 
 black:
 	black -l 86 $$(find * -name '*.py')
--- photos/models.py
+++ photos/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Photo(models.Model):
@@ -6,6 +7,8 @@ class Photo(models.Model):
     subtitle = models.CharField(max_length=250)
     path = models.CharField(max_length=250)
     mime_type = models.CharField(max_length=25)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
